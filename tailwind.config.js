cad794d59c9f3a3b9cb09c2280246449e4a57714
karem-dev <karemmahmoud.dev@gmail.com>
2026-02-20 15:24:03 +0200

    lets go

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,11 @@ export default {
       colors: {
         // You can add custom colors here if needed
       },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      },
     },
   },
   plugins: [],
-} 
+}
 
